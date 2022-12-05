338ac98c7311d5877b5fb7b2082d7109d0e86721
magikarp01 <philliphguo@gmail.com>
2022-12-04 23:10:15 -0500

    Testing the portfolio class

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import numpy as np
+from factor_portfolio import Portfolio
+
+def test_factor_portfolio():
+    
 
 # 2 factors, 4 days
 factor_changes = np.array([[5, 10, -3, 8], [9, -12, -3, -6]])
-print(factor_changes.shape)
-print(factor_changes.mean(axis=1))
+# print(factor_changes.shape)
+# print(factor_changes.mean(axis=1))
 
 # 3 stocks, 2 factors -> 2x3
 betas = np.array([[5, 10, 5], [9, -12, -3]])
@@ -13,5 +17,5 @@ weights = np.array([[1, 2, 3]])
 alphas = np.array([[3, 4, 5]])
 
 # print(np.dot(weights, np.transpose(betas)))
-print(np.dot(weights, np.transpose(alphas)))
-print(np.dot(weights, np.transpose(alphas)) + 4)
+# print(np.dot(weights, np.transpose(alphas)))
+# print(np.dot(weights, np.transpose(alphas)) + 4)
